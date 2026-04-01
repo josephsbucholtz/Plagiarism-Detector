05606b711e8c7150241996fb70ec554e7319ad71
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2026-04-01 00:27:42 -0700

    adding minHash
    we are up to minHash created signatures.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import os
 import sys
 import string
+from random import shuffle
 import numpy as np 
 
-declaration_text = "src/data/declaration.txt"
-declaration_variant = "src/data/declaration-variant.txt"
 
 def clean_document_data(inputFile: str):
     text = ""
@@ -27,7 +26,7 @@ def clean_document_data(inputFile: str):
     except Exception as e:
         print(e)
 
-def shingle(text: str, k=3):
+def shingles(text: str, k=3):
     shingle_set = set()
     word_list = text.split()
     for w in range(len(word_list) - k + 1):
@@ -35,14 +34,60 @@ def shingle(text: str, k=3):
 
     return shingle_set
 
+def create_hash(one_hot: list):
+    signature = []
+
+    for func in minhash_func:
+        for i in range(1, len(vocab)+1):
+            idx = func.index(i)
+            sig_val = one_hot[idx]
+            if sig_val == 1:
+                signature.append(idx)
+                break
+
+    return signature
+
+def create_minhash_func(vocab_size: int, minhash_size: int):
+    hashes = []
+
+    for _ in range(minhash_size):
+        vocab_shuffle = list(range(1, vocab_size + 1))
+        shuffle(vocab_shuffle)
+        hashes.append(vocab_shuffle)
+
+    return hashes
 
 
+# ---------------------- TESTING -------------------------------------
 
+#Texts
+declaration_text = "src/data/declaration.txt"
+declaration_variant = "src/data/declaration-variant.txt"
+repeated_text = "src/data/repeated.txt"
+
+#Clean Documents for processing data
 doc1 = clean_document_data(declaration_text)
-doc2 = clean_document_data(declaration_text)
-a = shingle(doc1)
-b = shingle(doc2)
+doc2 = clean_document_data(declaration_variant)
 
+#Create k-shingling for each doc
+a = shingles(doc1)
+b = shingles(doc2)
+
+#Create our Vocab
 vocab = a.union(b)
+vocab_list = list(vocab) 
 
+#Create our sparse vectors (later will become dense vectors)
+a1_hot = [1 if x in a else 0 for x in vocab] 
+b1_hot = [1 if x in b else 0 for x in vocab] 
+
+#Create dense vector
+minhash_func = create_minhash_func(len(vocab), 20)
+
+#Create Signatures
+a_sig = create_hash(a1_hot)
+b_sig = create_hash(b1_hot)
+
+print(a_sig)
+print(b_sig)
 
