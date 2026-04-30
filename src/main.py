3c718cc8ffc0d75e717451d6beedb9ca4b9fd3ce
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2026-04-30 16:48:42 -0700

    testing

--- src/main.py
+++ src/main.py
@@ -17,42 +17,50 @@ lincoln_text = "src/data/lincoln-address.txt"
 
 
 if __name__ == "__main__":
-    start = time.time()
-    results = utils.compare_all_files("src/essays/")
+    # start = time.time()
+    # results = utils.compare_all_files("src/essays/")
 
-    # Sort by highest estimated similarity
-    results.sort(key=lambda x: x["minhash_similarity"], reverse=True)
+    # # Sort by highest estimated similarity
+    # results.sort(key=lambda x: x["minhash_similarity"], reverse=True)
 
-    plagarised = set()
+    # plagarised = set()
 
-    for result in results:
-        if "error" in results:
-            print(results["error"])
+    # for result in results:
+    #     if "error" in results:
+    #         print(results["error"])
 
-        if result["minhash_similarity"] >= 0.30:
-            plagarised.add(result['file1'])
-            continue
+    #     if result["minhash_similarity"] >= 0.30:
+    #         plagarised.add(result['file1'])
+    #         continue
 
-        if result["minhash_similarity"] < 0.20:
-            continue 
+    #     if result["minhash_similarity"] < 0.20:
+    #         continue 
 
-        print("\n")
-        print(f"{os.path.basename(result['file1'])} vs {os.path.basename(result['file2'])}")
-        print("Estimated similarity:", result["minhash_similarity"])
-        print("Estimated similarity (%):", result["minhash_similarity"] * 100)
-        print("Jaccard similarity (REAL):", result["real_similarity"])
-        print("Jaccard similarity (REAL %):", result["real_similarity"] * 100)
-        print("\n")
-        print("-------------------------------------------------")
-        print("\n")
+    #     print("\n")
+    #     print(f"{os.path.basename(result['file1'])} vs {os.path.basename(result['file2'])}")
+    #     print("Estimated similarity:", result["minhash_similarity"])
+    #     print("Estimated similarity (%):", result["minhash_similarity"] * 100)
+    #     print("Jaccard similarity (REAL):", result["real_similarity"])
+    #     print("Jaccard similarity (REAL %):", result["real_similarity"] * 100)
+    #     print("\n")
+    #     print("-------------------------------------------------")
+    #     print("\n")
 
-        document.get_wordmap_doc(result["file1"], result["file2"], result["minhash_similarity"] * 100)
+    #     document.get_wordmap_doc(result["file1"], result["file2"], result["minhash_similarity"] * 100)
 
-    end = time.time()
+    # end = time.time()
 
-    for input in plagarised:
-        print(f"Plagiarised: {os.path.basename(input)}")
+    # for input in plagarised:
+    #     print(f"Plagiarised: {os.path.basename(input)}")
 
-    print(f"Total execution time: {end - start:.2f} seconds")
+    # print(f"Total execution time: {end - start:.2f} seconds")
     
+    results = utils.run_comparison(declaration_text, declaration_variant)
+    print("Doc 1 signature: ")
+    print(results["a_sig"])
+    print("\n")
+    print("Doc 2 signature: ")
+    print(results["b_sig"])
+    print(utils.compare_signatures(results["a_sig"], results["b_sig"]))
+    print(str(utils.compare_signatures(results["a_sig"], results["b_sig"]) * 100) + "% similar")
     
